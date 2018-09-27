7e7bcedfe25195f6a2dd5129fe05adcd28ca7515
Greg Stencel <greg.stencel@anthesisgroup.com>
2018-09-27 08:12:41 +0100

    vimrc

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ if [ ! -d "$(eval echo ~$USER)/.vim" ]; then
 fi
 mkdir -p "$(eval echo ~$USER)/.vim/cache/"{backup,session,swap,tags,undo,view,notes};
 git clone https://github.com/Shougo/dein.vim.git $(eval echo ~$USER)/.vim/dein.vim
-git clone https://grzegorz_stencel@bitbucket.org/grzegorz_stencel/vim-python-ide.git $(eval echo ~$USER)/.vim/cache/vim-python-ide
+git clone https://github.com/greg4fun/vim-python-ide.git $(eval echo ~$USER)/.vim/cache/vim-python-ide
 cp $(eval echo ~$USER)/.vim/cache/vim-python-ide/.vimrc $(eval echo ~$USER)
 vim -c "call dein#install()"
 git clone https://github.com/powerline/fonts.git
